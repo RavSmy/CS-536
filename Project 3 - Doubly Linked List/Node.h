e2d782a941cef9b2911f94e91523a0f69141c042
Ravela Smyth <RavSmy@users.noreply.github.com>
2017-10-05 14:04:57 -0700

    Update Node.h

--- Project 3 - Doubly Linked List/Node.h
+++ Project 3 - Doubly Linked List/Node.h
@@ -4,22 +4,25 @@
 #include <string>
 using namespace std;
 
-typedef int ItemType;
+typedef char ItemType;
 
 class Node
 {
 private:
-   ItemType  item;  // A data item
-   Node*     next;  // Pointer to next node
-   
+	ItemType  item;  // A data item
+	Node*	  prev;  // Pointer to prev node
+	Node*     next;  // Pointer to next node
+
 public:
-   Node();
-   Node(const ItemType& anItem);
-   Node(const ItemType& anItem, Node* nextNodePtr);
-   void setItem(const ItemType& anItem);
-   void setNext(Node* nextNodePtr);
-   ItemType getItem() const ;
-   Node* getNext() const ;
+	Node();
+	Node(const ItemType& anItem);
+	Node(const ItemType& anItem, Node* nextNodePtr);
+	void setItem(const ItemType& anItem);
+	void setNext(Node* nextNodePtr);
+	void setPrev(Node* nextNodePtr);
+	ItemType getItem() const;
+	Node* getNext() const;
+	Node* getPrev() const;
 }; // end Node
 
 #endif
