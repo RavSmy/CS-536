bcfe5574e89eb46c1db66f501491d580010bbca4
Ravela Smyth <RavSmy@users.noreply.github.com>
2017-10-05 14:04:44 -0700

    Update Node.cpp

--- Project 3 - Doubly Linked List/Node.cpp
+++ Project 3 - Doubly Linked List/Node.cpp
@@ -1,34 +1,45 @@
 #include "Node.h"
 
-Node::Node() : next(nullptr)
+Node::Node() : next(nullptr), prev(nullptr)
 {
 } // end default constructor
 
-Node::Node(const ItemType& anItem) : item(anItem), next(nullptr)
+Node::Node(const ItemType& anItem) : item(anItem), next(nullptr), prev(nullptr)
 {
 } // end constructor
 
 Node::Node(const ItemType& anItem, Node* nextNodePtr) :
-                item(anItem), next(nextNodePtr)
+	item(anItem), next(nextNodePtr)
 {
 } // end constructor
 
 void Node::setItem(const ItemType& anItem)
 {
-   item = anItem;
+	item = anItem;
 } // end setItem
 
 void Node::setNext(Node* nextNodePtr)
 {
-   next = nextNodePtr;
+	next = nextNodePtr;
 } // end setNext
+
+void Node::setPrev(Node* prevNodePtr)
+{
+	prev = prevNodePtr;
+} // end setPrev
+
 
 ItemType Node::getItem() const
 {
-   return item;
+	return item;
 } // end getItem
 
 Node* Node::getNext() const
 {
-   return next;
+	return next;
 } // end getNext
+
+Node* Node::getPrev() const
+{
+	return prev;
+} // end getPrev
